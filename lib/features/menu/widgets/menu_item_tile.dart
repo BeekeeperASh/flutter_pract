94dea36cef277aabe40d7ec8af86991aa6e35d46
Beekeeper <alvshinev@gmail.com>
2025-10-17 18:29:32 +0300

    Создание виджета menu_item_tile.dart

---
+++ lib/features/menu/widgets/menu_item_tile.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../models/menu_item.dart';
+
+class MenuItemTile extends StatelessWidget {
+  final MenuItem item;
+  final VoidCallback onAddToCart;
+
+  const MenuItemTile({
+    super.key,
+    required this.item,
+    required this.onAddToCart,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Colors.pink.shade100,
+          child: const Icon(Icons.cake, color: Colors.pink),
+        ),
+        title: Text(item.title),
+        subtitle: Text(item.description),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('${item.price.toInt()} ₽', style: const TextStyle(fontWeight: FontWeight.bold)),
+            ElevatedButton(
+              onPressed: onAddToCart,
+              child: const Text('В корзину'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
