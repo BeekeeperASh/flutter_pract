dc50871eb90b7fa7a67820fdf24e279af91534a2
Beekeeper <alvshinev@gmail.com>
2025-10-17 16:57:18 +0300

    Создание структуры menu_item.dart

---
+++ lib/features/menu/models/menu_item.dart
@@ -0,0 +1,15 @@
+class MenuItem {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageAsset;
+
+  const MenuItem({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageAsset,
+  });
+}
