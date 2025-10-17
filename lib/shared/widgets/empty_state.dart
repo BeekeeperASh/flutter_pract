d4191226a3ba2237e5b9b130d4f8a81bfd95d106
Beekeeper <alvshinev@gmail.com>
2025-10-17 18:53:53 +0300

    Создание виджета empty_state.dart

---
+++ lib/shared/widgets/empty_state.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class EmptyState extends StatelessWidget {
+  final String message;
+  final IconData icon;
+
+  const EmptyState({
+    super.key,
+    required this.message,
+    this.icon = Icons.info_outline,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: 64,
+            color: Colors.grey.shade400,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            message,
+            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+              color: Colors.grey.shade600,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
