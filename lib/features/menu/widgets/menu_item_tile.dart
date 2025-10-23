6bb566cbbe1dc0942f4883b787113cbb591f880b
Beekeeper <alvshinev@gmail.com>
2025-10-23 20:15:04 +0300

    Реализация CachedNetworkImage в файле menu_item_tile.dart

--- lib/features/menu/widgets/menu_item_tile.dart
+++ lib/features/menu/widgets/menu_item_tile.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import '../models/menu_item.dart';
 
@@ -16,16 +17,24 @@ class MenuItemTile extends StatelessWidget {
     return Card(
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: ListTile(
-        leading: CircleAvatar(
-          backgroundColor: Colors.pink.shade100,
-          child: const Icon(Icons.cake, color: Colors.pink),
+        leading: CachedNetworkImage(
+          imageUrl: item.imageAsset,
+          progressIndicatorBuilder: (context, url, error) =>
+              const CircularProgressIndicator(),
+          errorWidget: (context, url, error) => CircleAvatar(
+            backgroundColor: Colors.pink.shade100,
+            child: const Icon(Icons.cake, color: Colors.pink),
+          ),
         ),
         title: Text(item.title),
         subtitle: Text(item.description),
         trailing: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('${item.price.toInt()} ₽', style: const TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              '${item.price.toInt()} ₽',
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
             ElevatedButton(
               onPressed: onAddToCart,
               child: const Text('В корзину'),
